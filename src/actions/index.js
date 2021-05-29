82388d18073cb003a53d9337e433829ee3041847
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-29 14:53:44 +0200

    Use abbreviated notation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,9 @@
 import jsonPlaceholderTypicode from "../api/jsonPlaceholderTypicode";
 
-export const fetchPosts = () => {
-    return async function (dispatch, getState) {
+export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholderTypicode.get("/posts");
         dispatch({
             type: "FETCH_POSTS",
             payload: response
         })
-    }
-}
+    };
