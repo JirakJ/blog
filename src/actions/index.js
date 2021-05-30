6eb7bcd76cfe1337daed7089c36b47d20064f1ae
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-30 14:22:09 +0200

    Add fetching for user

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,14 @@ export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholderTypicode.get("/posts");
         dispatch({
             type: "FETCH_POSTS",
-            payload: response
+            payload: response.data
+        })
+    };
+
+export const fetchUser = (userId) => async dispatch => {
+        const response = await jsonPlaceholderTypicode.get(`/users/${userId}`);
+        dispatch({
+            type: "FETCH_USER",
+            payload: response.data
         })
     };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import {combineReducers} from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-    justDummyReducerToPreventErrorMessage: () => "Remove me after some reducer added"
+    posts: postsReducer,
+    users: usersReducer
 })
