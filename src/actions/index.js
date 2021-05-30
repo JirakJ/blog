ccc183a7565827a0b7ded68e40897ff69eafbb82
Ing. Jakub Jirák <jakub@jakubjirak.com>
2021-05-30 14:34:23 +0200

    Add lodash memoize to prevent multiple API requests for already known data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholderTypicode from "../api/jsonPlaceholderTypicode";
 
 export const fetchPosts = () => async dispatch => {
@@ -8,10 +9,15 @@ export const fetchPosts = () => async dispatch => {
         })
     };
 
-export const fetchUser = (userId) => async dispatch => {
-        const response = await jsonPlaceholderTypicode.get(`/users/${userId}`);
-        dispatch({
-            type: "FETCH_USER",
-            payload: response.data
-        })
+export const fetchUser = (userId) => dispatch => {
+        _fetchUser(userId, dispatch);
     };
+
+//private function
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+    const response = await jsonPlaceholderTypicode.get(`/users/${userId}`);
+    dispatch({
+        type: "FETCH_USER",
+        payload: response.data
+    })
+});
